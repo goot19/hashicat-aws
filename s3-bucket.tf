04e029786191804262cf49e5a665417e8a6f442c
goot19 <andrew.leibundgut@connectbyamfam.com>
2021-07-12 19:05:13 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/NewGoot/s3-bucket/aws"
   version = "2.6.0"  
-  bucket = "my-s3-bucket"
 
   # insert required variables here
   bucket_prefix = "NewGoot"
